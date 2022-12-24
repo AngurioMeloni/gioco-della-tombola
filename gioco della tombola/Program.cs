b51fb4c1f9a1aa91bca9da9fc219892adfb2daf6
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2022-12-24 23:08:14 +0100

    dichiarazione variabili

--- gioco della tombola/Program.cs
+++ gioco della tombola/Program.cs
@@ -10,8 +10,18 @@ namespace gioco_della_tombola
     {
         static void Main(string[] args)
         {
-            //dichiarazioni
-            int a;
+            //dichiarazioni delle variabili per la dimensione delle cartelle e del tabellone
+            int TabRow = 9,TabColumn = 10;
+            int CartRow = 3, CartColumn = 5;
+            //dichiarazione delle variabili per il posizionamento del tabellone e cartelle
+            int xT = 40, yT = 1, xCart1 = 20, yCart1 = 15, xCart2 = 40, yCart2 = 15;
+            //dichiarazione del random
+            Random r = new Random();
+            //dichiarazione matrici
+            int[,] tabella = new int[TabRow, TabColumn];
+            int[,] Cartella1 = new int[CartRow, CartColumn];
+            int[,] Cartella2= new int[CartRow, CartColumn];
+            //funzione della stampa della tabella
         }
     }
 }

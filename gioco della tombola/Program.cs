5adb147fbbb391938bf675cbd207866192246f64
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-01-09 15:54:20 +0100

    correzione cartella secondo giocatore

--- gioco della tombola/Program.cs
+++ gioco della tombola/Program.cs
@@ -16,21 +16,21 @@ namespace gioco_della_tombola
             int TabN = 1;
             int CartRow = 3, CartColumn = 9;
             //dichiarazione delle variabili per il posizionamento del tabellone e cartelle
-            int xT = 40, yT = 1, xC1 = 20, yC1 = 13, xC2 = 76, yC2 = 13;
+            int xT = 40, yT = 1;
             //dichiarazione delle matrici
             int[,] tabella = new int[TabRow, TabColumn];
             int[,] C1 = new int[CartColumn, CartRow];
-            int[,] C2 = new int[CartRow, CartColumn];
+            int[,] C2 = new int[CartColumn, CartRow];
             //richiamo della funzione per la tabella
             Tabella();
             //richiamo alla funzione di caricamento della prima tabella
             GC1();
             //richiamo delle funzione di stampa per la prima cartella
+            GC2();
+            //richiamo alla funzione di caricamento della prima tabella
             FC1();
             //richiamo delle funzione di stampa per la seconda cartella
             FC2();
-            //richiamo della funzione di estrazione
-            Es();
             Console.ReadKey();
             //funzione della stampa della tabella
             void Tabella()
@@ -84,13 +84,52 @@ namespace gioco_della_tombola
                         {
                             arrayD[b] = false;
                         }
+                    }
+                }
+                return 0;
+            }
+            int GC2()
+            {
+                int numero2;
+                Random r = new Random();
+                bool[] t = new bool[90];
+                for (int a = 0; a < 3; a++)
+                {
+                    bool[] arrayD = new bool[10];
+                    for (int a2 = 0; a2 < 5; a2++)
+                    {
+                        do
+                        {
+                            numero2 = r.Next(1, 91);
+                            if (numero2 == 90)
+                            {
+                                a2--;
 
+                            }
+
+                        } while (t[numero2 - 1] == true || arrayD[numero2 / 10] == true);
+                        t[numero2 - 1] = true;
+                        arrayD[numero2 / 10] = true;
+                        if (numero2 == 90)
+                        {
+                            C2[8, a] = 90;
+                        }
+                        else
+                        {
+                            C2[numero2 / 10, a] = numero2;
+                        }
+                        for (int b = 0; b < CartColumn; b++)
+                        {
+                            arrayD[b] = false;
+                        }
                     }
                 }
                 return 0;
             }
             void FC1()
             {
+                int xC1 = 0;
+                int yC1 = 12;
                 Console.SetCursorPosition(xC1, yC1);
                 Console.WriteLine("Cartella del primo giocatore");
                 yC1++;
@@ -101,7 +140,7 @@ namespace gioco_della_tombola
                     if (i % 2 == 1)
                     {
                         Console.SetCursorPosition(xC1, yC1);
-                        Console.WriteLine("------------------------------");
+                        Console.WriteLine("-----------------------");
                     }
                     else
                     {
@@ -128,35 +167,47 @@ namespace gioco_della_tombola
                     }
                 }
             }
-
-
-
-
             void FC2()
             {
-                Console.SetCursorPosition(68, 11);
-                Random rnd = new Random();
-                Console.WriteLine("Cartella del secondo giocatore");
-                for (int i = 0; i < CartRow; i++)
+                int xC2 = 70;
+                int yC2 = 12;
+                Console.SetCursorPosition(xC2, yC2);
+                Console.WriteLine("Cartella del secondo giocatore ");
+                yC2++;
+                for (int i = 0; i < 5; i++)
                 {
-                    Console.WriteLine();
-                    Console.SetCursorPosition(xC2, yC2);
-                    for (int i2 = 0; i2 < CartColumn; i2++)
-                    {
-                        for (int j = 1; j <= 10; j++)
-                        {
-                            C2[i, i2] = rnd.Next(1, 91);
-                        }
-                        Console.Write(C2[i, i2].ToString("D2") + " ");
-                    }
+                    xC2 = 70;
                     yC2++;
+                    if (i % 2 == 1)
+                    {
+                        Console.SetCursorPosition(xC2, yC2);
+                        Console.WriteLine("---------------------------");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(xC2, yC2);
+                        for (int i2 = 0; i2 < 9; i2++)
+                        {
+                            if (C2[i2, i / 2 + i % 2] != 0)
+                            {
+                                Console.Write($"{C2[i2, i / 2 + i % 2]} ");
+                            }
+                            else
+                            {
+                                if (i2 == 0)
+                                {
+                                    Console.Write("  ");
+                                }
+                                else
+                                {
+                                    Console.Write("   ");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                    }
                 }
             }
-            void Es()
-            {
-
-            }
-
         }
     } 
 }
